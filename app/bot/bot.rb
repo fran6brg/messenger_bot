fb7ab6e85307d6976ac825bce360402177b8216b
francis berger <frcs.berger@gmail.com>
2018-06-30 14:40:14 +0200

    ok

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,5 +5,6 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN_FB'
 
 Bot.on :message do |message|
   message.reply(text: 'Hello, donne moi un @identifiant ou bien un #hashtag et je te dis ce qu\' en pense Twitter !')
-  message.reply(text: application.controller.search_for("a string"))
+  message = application.controller.search_for("a string")
+  message.reply(text: message || "message")
 end
