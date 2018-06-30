76fa9d2b3aa1ab3191d08c5fcb303babb7252a80
francis berger <frcs.berger@gmail.com>
2018-06-30 15:43:37 +0200

    ok

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,5 +4,20 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, donne moi un @identifiant ou bien un #hashtag et je te dis ce qu en pense Twitter !')
+  if message.text.nil?
+    message.reply(text: 'Hello, donne moi un @identifiant ou bien un #hashtag et je te dis ce qu\' en pense Twitter :).')
+  elsif message.text.include ("@" || "#")
+    message.reply(text: 'Ok, je t\'envoie les résultats de mon analyse de "#{message.text}" asap.')
+  else
+    message.reply(
+      text: 'Human, who is your favorite bot?',
+      quick_replies: [
+        {
+          content_type: 'text',
+          title: 'You are!',
+          payload: 'HARMLESS'
+        }
+      ]
+    )
+  end
 end
