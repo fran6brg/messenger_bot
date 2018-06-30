83f670fe6e0a94241e26872a37b6af283296d9ae
francis berger <frcs.berger@gmail.com>
2018-06-30 18:48:54 +0200

    ok last v

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -4,13 +4,20 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, donne moi un @id ou bien un #hashtag et je te dis ce qu\' en pense Twitter :).')
+  message.reply(text: 'Hello, donne moi un @id ou bien un #hashtag et je te dis ce qu\'en pense Twitter :).')
   if message.text.include? ("@" || "#")
     message.reply(text: 'Ok bro, je t\'envoie les résultats de mon analyse asap.')
     # client = TwitterHelper.get_client
     results = TwitterHelper.search_for(message.text)
-    results.each do |result|
-      message.reply(text: "Positif : #{result}")
+    sum = 0
+    if results.nil?
+      message.reply(text: 'Aucun tweet ne match ta requête :/.')
+    else
+      results.each { |a| sum+=a }
+      message.reply(text: 'Il y a #{sum} tweets ;).')
+      results.each do |result|
+        message.reply(text: "Positif : #{result}")
+      end
     end
   else
     message.reply(
